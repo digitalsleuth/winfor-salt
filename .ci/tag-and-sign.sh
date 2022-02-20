5e56b00bb6c481ffa5038daf4abce053e4dbcfa9
red <corey@digitalsleuth.ca>
2022-02-20 01:52:14 +0000

    Add tag and sign script, update README

---
+++ .ci/tag-and-sign.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -x
+
+set -x
+
+if [ "x`which jq`" == "x" ]; then
+  echo "jq is required to use this script"
+  exit 10
+fi
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 TAG_NAME"
+  exit 1
+fi
+
+if [ "x${GITHUB_ACCESS_TOKEN}" == "x" ]; then
+  echo "GITHUB_ACCESS_TOKEN env var must be set for authentication"
+  exit 5
+fi
+
+TAG_NAME=$1
+PRERELEASE=false
+
+`echo $TAG_NAME | grep -q "rc"`
+if [ $? -eq 0 ]; then
+  PRERELEASE="true"
+fi
+
+STASH_RESULTS=`git stash -u`
+VERSION_FILE="winfor/VERSION"
+
+if [ "`cat ${VERSION_FILE}`" != "${TAG_NAME}" ]; then
+  echo "==> Updating Release Version"
+  rm -f ${VERSION_FILE}
+  echo "$TAG_NAME" > ${VERSION_FILE}
+  git add ${VERSION_FILE}
+  git commit -m "Updating VERSION to $TAG_NAME"
+  git push origin master
+
+  echo "==> Tagging Repository"
+  git tag $TAG_NAME
+
+  echo "==> Pushing Tags to Remote"
+  git push origin --tags
+
+  echo "==> Sleeping, waiting for tar.gz file"
+  sleep 3
+fi
+
+echo "==> Creating GitHub Release"
+RELEASE_ID=`curl -XPOST -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -q https://api.github.com/repos/digitalsleuth/winfor-salt/releases -d "{\"tag_name\": \"$TAG_NAME\", \"prerelease\": $PRERELEASE}" | jq .id`
+
+echo "==> Downloading tar.gz file for tag from GitHub"
+curl -qL -o /tmp/winfor-salt-${TAG_NAME}.tar.gz https://github.com/digitalsleuth/winfor-salt/archive/$TAG_NAME.tar.gz
+
+echo "==> Generating SHA256 of tar.gz"
+shasum -a 256 /tmp/winfor-salt-$TAG_NAME.tar.gz > /tmp/winfor-salt-$TAG_NAME.tar.gz.sha256
+
+echo "==> Generating GPG Signature of SHA256"
+gpg --armor --clearsign --digest-algo SHA256 -u 4CF992E3 /tmp/winfor-salt-$TAG_NAME.tar.gz.sha256
+
+echo "==> Generating GPG Signature of tar.gz file"
+gpg --armor --detach-sign -u 4CF992E3 /tmp/winfor-salt-$TAG_NAME.tar.gz
+
+echo "==> Uploading winfor-salt-$TAG_NAME.tar.gz.sha256"
+curl -XPOST -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Content-Type: text/plain" -q "https://uploads.github.com/repos/digitalsleuth/winfor-salt/releases/${RELEASE_ID}/assets?name=winfor-salt-${TAG_NAME}.tar.gz.sha256" --data-binary @/tmp/winfor-salt-$TAG_NAME.tar.gz.sha256
+
+echo "==> Uploading winfor-salt-$TAG_NAME.tar.gz.sha256.asc"
+curl -XPOST -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Content-Type: text/plain" -q "https://uploads.github.com/repos/digitalsleuth/winfor-salt/releases/${RELEASE_ID}/assets?name=winfor-salt-${TAG_NAME}.tar.gz.sha256.asc" --data-binary @/tmp/winfor-salt-$TAG_NAME.tar.gz.sha256.asc
+
+echo "==> Uploading winfor-salt-$TAG_NAME.tar.gz.asc"
+curl -XPOST -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -H "Content-Type: text/plain" -q "https://uploads.github.com/repos/digitalsleuth/winfor-salt/releases/${RELEASE_ID}/assets?name=winfor-salt-${TAG_NAME}.tar.gz.asc" --data-binary @/tmp/winfor-salt-$TAG_NAME.tar.gz.asc
+
+rm /tmp/winfor-salt-${TAG_NAME}.tar.gz
+rm /tmp/winfor-salt-$TAG_NAME.tar.gz.sha256
+rm /tmp/winfor-salt-$TAG_NAME.tar.gz.sha256.asc
+rm /tmp/winfor-salt-$TAG_NAME.tar.gz.asc
+
+if [ "${STASH_RESULTS}" != "No local changes to save" ]; then
+  git stash pop
+fi
--- README.md
+++ README.md
@@ -1,19 +1,41 @@
-# winfor-salt - WIN-FOR
+# WIN-FOR Salt States
 Windows Forensics VM Salt States
 
-The design behind this is to use a barebones Windows 10 VM (preferably 1909 and higher),
-install [Git](https://git-scm.com/download/win), then install [Saltstack 3004](https://repo.saltproject.io/windows/Salt-Minion-3004-3-Py3-AMD64-Setup.exe)
-using the following command:
+The design behind this is to use a barebones Windows 10 VM (preferably 1909 and higher to support WSLv2).
+Once configured, and activated (to support customization features), then you can use the install.ps1 file to
+install all of the packages.
 
-`Salt-Minion-3004-3-Py3-AMD64-Setup.exe /S /master=localhost /minion-name=localhost`
+The install requires that the `Set-ExecutionPolicy` feature be set to allow the script to run, at least twice, depending on your choices.
+This is best left to you to decide what is acceptable in your organization.
 
-Once the install of the above are completed, you can test the states out by doing the following:
-Open an Administrator Command Prompt and run:
+## Usage
+```text
+Usage:
+  .\install.ps1 -user <user> -mode <mode> -wsl -update -upgrade
+  .\install.ps1 -version
+
+Options:
+  -user <user>	User used for configuration of VM and software, default is current user
+  -mode <mode>	WIN-FOR Install mode (addon (applications only, no customization); dedicated (everything))
+  -wsl		Also install the Windows Subsystem for Linux v2 with SIFT and REMnux installed
+		This will cause a reboot, and will be run after all other installs are complete
+		Can be used separately one a full install and customization
+  -version	Provides the current version of WIN-FOR, will display `not installed` on first run or
+		on subsequent runs where the installation is incomplete
+  -update	Feature coming soon
+  -upgrade	Feature coming soon
+```
+
+## Issues
+
+A new [WIN-FOR Repo](https://github.com/digitalsleuth/winfor) will be set up soon, for now, issues can be raised
+in this repo.
+
+## What the install.ps1 does
+
+Installs [Saltstack 3004](https://repo.saltproject.io/windows/Salt-Minion-3004-3-Py3-AMD64-Setup.exe) then installs 
+[Git](https://git-scm.com/download/win), then runs the following commands:
 ```
 git clone https://github.com/digitalsleuth/winfor-salt/ "C:\ProgramData\Salt Project\Salt\srv\salt"
-salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls winfor.dedicated pillar="{'winfor_user': '<username>'}" --log-file="C:\saltstack.log" --log-file-level=debug --out-file="C:\saltstack.log" --out-file-append
+salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls winfor.$mode pillar="{'winfor_user': '$user'}" --log-file="C:\winfor-saltstack.log" --log-file-level=debug --out-file="C:\winfor-saltstack.log" --out-file-append
 ```
-
-NOTE: Replace `<username>` with the desired username for the VM. If the user does not exist, it will be created.
-
-Once an installer has been created, it will be made available!
